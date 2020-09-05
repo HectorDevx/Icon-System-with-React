3ba3dfe7814b00cc596c7dc66acf0322bcf61574
Héctor Reyes <h.reyes@idventiva.com>
2020-09-05 18:41:39 -0500

    Iconos implementados

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -1,7 +1,17 @@
 import React from "react";
 
+import Icon from "./icons/Icon.jsx";
+
 const HelloWorld = () => {
-  return <h3>Hello World</h3>;
+  return (
+    <>
+      <h3>Hello World</h3>
+      <p>
+        <Icon svg="facebook" classes="svg-icon" title="Facebook" />
+        <span>Danos like en facebook</span>
+      </p>
+    </>
+  );
 };
 
 export default HelloWorld;
